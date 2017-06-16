e7c51bf148b18ccd0adbf5d8cbe9f3d252116750
aaa8414 <18616023648@163.com>
2017-06-16 15:16:57 +0800

    add...

--- app/controllers/law_controller.rb
+++ app/controllers/law_controller.rb
@@ -1,4 +1,9 @@
 class LawController < ApplicationController
   def show
+    if params[:id] == "101"
+      render "law/101"
+    else
+      render "law/show"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'news/show'
+
   get 'law/show'
 
   get 'about/show'
@@ -21,6 +23,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  get 'law/show/:id' => 'law#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
