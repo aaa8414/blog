4df3df0bffa7e707fdd6c1a90b7a1e85cd0cb0d3
aaa8414 <18616023648@163.com>
2017-06-19 16:53:54 +0800

    fix bug redirect

--- app/controllers/law_controller.rb
+++ app/controllers/law_controller.rb
@@ -2,6 +2,8 @@ class LawController < ApplicationController
   def show
     if params[:id] == "101"
       render "law/101"
+    elsif params[:id] == "102"
+      render "law/102"
     else
       render "law/show"
     end
