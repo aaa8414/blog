7da730f15af16f095c8c56cd16aa439628e0aa20
aaa8414 <18616023648@163.com>
2017-06-19 21:15:44 +0800

    fix icp number

--- app/controllers/law_controller.rb
+++ app/controllers/law_controller.rb
@@ -4,6 +4,18 @@ class LawController < ApplicationController
       render "law/101"
     elsif params[:id] == "102"
       render "law/102"
+    elsif params[:id] == "103"
+      render "law/103"
+    elsif params[:id] == "104"
+      render "law/104"
+    elsif params[:id] == "105"
+      render "law/105"
+    elsif params[:id] == "106"
+      render "law/106"
+    elsif params[:id] == "107"
+      render "law/107"
+    elsif params[:id] == "108"
+      render "law/108"
     else
       render "law/show"
     end
